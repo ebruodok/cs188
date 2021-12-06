29d7219b9ca17abe92adc04672127a0a9dcc3880
Ebru Odok <ebruodok@gmail.com>
2021-12-05 18:40:00 -0800

    q7 done

--- tracking/inference.py
+++ tracking/inference.py
@@ -399,7 +399,11 @@ class ParticleFilter(InferenceModule):
         gameState.
         """
         "*** YOUR CODE HERE ***"
-        raiseNotDefined()
+        newParticles = []
+        for oldPos in self.particles:
+            newPosDist = self.getPositionDistribution(gameState, oldPos)
+            newParticles.append(newPosDist.sample())
+        self.particles = newParticles
 
     def getBeliefDistribution(self):
         """
